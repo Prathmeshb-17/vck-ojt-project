71c5d152f2bf39206a422545a0b72028f5888a91
sanku8605 <sanketbhosale309@gmail.com>
2025-06-26 13:44:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import './styles/Pages.css';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import CoursesPage from './pages/CoursesPage';
+import ContactPage from './pages/ContactPage';
+import Admissionpage from './pages/Admissionpage';
+const App=()=>{
+  return(
+    <div>
+      <Router>
+        <Routes>
+            <Route path="/" element={<HomePage/>}/>
+          <Route path="/home" element={<HomePage/>}/>
+          <Route path="/about" element={<AboutPage/>}/>
+          <Route path="/courses" element={<CoursesPage/>}/>
+           <Route path="/contact" element={<ContactPage/>}/>
+           <Route path="/admission" element={<Admissionpage/>}/>
+
+        </Routes>
+      </Router>
+  </div>
+  )
+  
+}
+export default App;
